49b97d80ddc172a55bc2f7773e385361ed757f9f
freezsir <70756979+freezsir@users.noreply.github.com>
2021-12-19 17:30:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+########### 更改大雕源码（可选）###########
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+# 拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+########### 安装smartdns（必选）###########
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
