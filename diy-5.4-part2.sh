c8968b59dbf212c716018bf935826ffb391e6386
freezsir <70756979+freezsir@users.noreply.github.com>
2022-03-09 22:57:42 +0800

    Update diy-5.4-part2.sh

--- diy-5.4-part2.sh
+++ diy-5.4-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ########### 更改大雕源码（可选）###########
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt Compiled by Xiaosage '/g" ./package/lean/default-settings/files/zzz-default-settings
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
