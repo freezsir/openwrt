13d732f4ae480d03f7abc4c908a5bd05e96cfa4b
freezsir <70756979+freezsir@users.noreply.github.com>
2022-03-17 20:34:39 +0800

    Update diy-5.15-part2.sh

--- diy-5.15-part2.sh
+++ diy-5.15-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ########### 更改大雕源码（可选）###########
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt Compiled by Xiaosage '/g" ./package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt Compiled by Dockerman '/g" ./package/lean/default-settings/files/zzz-default-settings
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
 # 拉取 argon 原作者的源码
